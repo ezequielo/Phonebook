242175db1c3b833d2f61064891921c6f0132e13b
ezequielo <ezequielra1@gmail.com>
2014-10-27 17:09:42 +0100

    add contact logic

--- phonebook.py
+++ phonebook.py
@@ -24,10 +24,15 @@ def phonebook_key(phonebook_name):
     """Constructs a Datastore key for a Contact entity."""
     return ndb.Key('Guestbook', phonebook_name)
 
+def get_contacts(user_id):
+    phonebook_query = Contact.query(ancestor=phonebook_key(user_id)).order(-Contact.date_add)
+    return phonebook_query.fetch(10)
+
+
 
 class Contact(ndb.Model):
     """Models an individual Contact."""
-    name = ndb.StringProperty
+    cname = ndb.StringProperty(indexed=False)
     phone = ndb.StringProperty(indexed=False)
     email = ndb.StringProperty(indexed=False)
     date_add = ndb.DateTimeProperty(auto_now_add=True)
@@ -52,9 +57,7 @@ class MainPage(webapp2.RequestHandler):
 class Profile(webapp2.RequestHandler):
     def get(self):
         if users.get_current_user():
-            phonebook_query = Contact.query(
-                ancestor=phonebook_key(users.get_current_user().user_id())).order(-Contact.date_add)
-            contacts = phonebook_query.fetch(10)
+            contacts = get_contacts(users.get_current_user().user_id())
             url = users.create_logout_url(self.request.uri)
             url_linktext = 'Logout'
             template = JINJA_ENVIRONMENT.get_template('profile.html')
@@ -68,8 +71,44 @@ class Profile(webapp2.RequestHandler):
         else:
             self.redirect('/')
 
+    def post(self):
+        self.redirect('/new_contact')
+
+
+
+class NewContact(webapp2.RequestHandler):
+    def get(self):
+
+        if users.get_current_user():
+            contacts = get_contacts(users.get_current_user().user_id())
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
+            template = JINJA_ENVIRONMENT.get_template('add.html')
+            template_values={
+                'url':url,
+                'url_linktext':url_linktext,
+                'contacts':contacts,
+            }
+            self.response.write(template.render(template_values))
+        else:
+            self.redirect('/')
+
+    def post(self):
+        print 'dsfsdfds'
+        if users.get_current_user():
+            contact = Contact(parent=phonebook_key(users.get_current_user().user_id()))
+            contact.email = self.request.get('contact_email')
+            contact.cname = self.request.get('contact_name')
+            contact.phone = self.request.get('contact_phone')
+            contact.put()
+            self.redirect('/profile')
+
+        else:
+            self.redirect('/')
+
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/profile', Profile),
+    ('/new_contact', NewContact),
 ], debug=True)
